0161d1a77b1c1ca88c23a997794042d1895faff6
Manuem-dev <yemoletrader@gmail.com>
2026-03-31 22:46:18 +0000

    Update settings.py

--- sm/settings.py
+++ sm/settings.py
@@ -22,8 +22,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-j-)lebe!=m7!&sx#74yir(lsu#kp$hcriq&@lu(j3o8%#h4i@*')
@@ -94,8 +92,7 @@ else:
     }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,8 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
